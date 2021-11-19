032f223f755366ab21e60c8a74c233acd1feb30e
Andu404 <manuu307@gmail.com>
2021-11-19 15:07:33 -0300

    smtp on port 25 option added

--- send_mail.py
+++ send_mail.py
@@ -7,18 +7,51 @@ class SendMail:
     def __init__(self, receiver_email, message):
         smtp_server = smtp_server_  # "smtp.gmail.com"
         sender_email = sender_email_
-        port = email_port_  # 465 For SSL
+        port = port_  # 465 For SSL
         self.receiver_email = receiver_email
-        password = email_password_
+        password = password_
         self.message = message
         # Create a secure SSL context
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            try:
-                server.login(sender_email, password)
-            except:
-                print("Error login in")
-            else:
-                print("Logged in succesfully")
-                server.sendmail(sender_email, receiver_email, message)
-                print("Email sent to:", receiver_email_)
+        if port == 465:
+            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                try:
+                    server.login(sender_email, password)
+                except:
+                    print("Error login in")
+                else:
+                    print("Logged in succesfully")
+                    server.sendmail(sender_email, receiver_email, message)
+                    print("Email sent to:", receiver_email_)
+        elif port == 25:
+            # send mail without ssl on port 25 (default)
+            with smtplib.SMTP(smtp_server, port) as server:
+                try:
+                    server.login(sender_email, password)
+                except:
+                    print("Error login in")
+                else:
+                    print("Logged in succesfully")
+                    server.sendmail(sender_email, receiver_email, message)
+                    print("Email sent to:", receiver_email_)
+                    server.quit()
+                    print("Quit")
+                    server.close()
+                    print("Closed")
+        elif port == 587:
+            # send mail without ssl on port 587 (default)
+            with smtplib.SMTP(smtp_server, port) as server:
+                try:
+                    server.login(sender_email, password)
+                except:
+                    print("Error login in")
+                else:
+                    print("Logged in succesfully")
+                    server.sendmail(sender_email, receiver_email, message)
+                    print("Email sent to:", receiver_email_)
+                    server.quit()
+                    print("Quit")
+                    server.close()
+                    print("Closed")
+        else:
+            print("Error: port not supported")
